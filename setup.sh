8a1e48e061fb4426f9c1188b8a82a9b45953d488
Solomon Hykes <solomon@dagger.io>
2025-02-04 13:33:15 -0800

    setup.sh: don't break when local module doesn't build
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ fi
 echo "Building CLI and starting engine. To connect, type:"
 echo "_EXPERIMENTAL_DAGGER_RUNNER_HOST=tcp://localhost:1234 ~/bin/dagger-llm shell"
 
-dagger shell <<EOF
+dagger shell --no-mod <<EOF
 github.com/shykes/dagger@llm | cli | binary --platform=current | export "$HOME/bin/dagger-llm"
 github.com/shykes/dagger@llm | engine | service llm | up
 EOF

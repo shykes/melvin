cbd6d8f25b773c7de3c9cc107876a36f772a9a9b
Solomon Hykes <solomon@dagger.io>
2025-02-05 00:33:41 -0800

    melvin: simple demo
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ demo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"dagger/demo/internal/dagger"
+)
+
+type Demo struct{}
+
+// Automate a Go programming task with a LLM
+// The input is a prompt and an optional starting point.
+// The output is the generated / modified source code in a containerized dev environment
+func (m *Demo) GoProgrammer(ctx context.Context,
+	// A starting point for the coder's workspace.
+	// Defaults to an empty directory
+	// +optional
+	start *dagger.Directory,
+	// A description of the Go programming task to perform
+	task string,
+) *dagger.Container {
+	workspace := dag.Workspace(dagger.WorkspaceOpts{
+		Start:   start,
+		Checker: dag.Go(dag.Directory()).Base().WithDefaultArgs([]string{"go", "build", "./..."}),
+	})
+	result := dag.
+		Llm().
+		WithWorkspace(workspace).
+		WithPrompt("You are a Go programmer. Use your workspace to accomplish the given task. Check your work with the 'check' tool").
+		Workspace()
+	return dag.Go(result.Dir()).Env()
+}
